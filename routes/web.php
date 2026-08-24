74faac91856d3401f923c531e4cb154654c920e4
Chali Musonda <mcmusonda@gmail.com>
2026-08-24 03:23:51 +0200

    route params and constraints

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function() {
+    return "<h1>Available jobs</h1>";
+})->name('jobs.index');
+
+Route::get('/posts/{id}', function($id) {
+    return 'Post ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('/posts/{id}/comments/{commentid}', function($id, $commentid) {
+    return "<h1>Post id " . $id . ", comment id " . $commentid;
+});
+// Route::post('/submit', function(){
+//     return "Submitted";
+// }); 
+
+// Route::match(['get', 'post'], '/submit', function() {
+//     return 'Submitted';
+// });
+
+// Route::get('/test', function(){
+//     $url = route('jobs.index');
+//     return "<a href='$url'>Click Here</a>";
+// });
+
+// Route::get('/api/users', function() {
+//     return [
+//        'name' => 'Mabvuto Phiri',
+//        'email' => 'mphiri@gmail.com' 
+//     ];
+// });
