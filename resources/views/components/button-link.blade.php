1e861a8c9d9b9be2bca189de5c19a375f4e631f7
Chali Musonda <mcmusonda@gmail.com>
2026-08-29 16:21:29 +0200

    5-8 - Button Link Component Challenge

---
+++ resources/views/components/button-link.blade.php
@@ -0,0 +1,16 @@
+@props([
+    'url' => '/',
+    'active' => true,
+    'icon' => null,
+    'bgColor' => 'bg-yellow-500',
+    'hoverClass' => 'hover:bg-yellow-600',
+    'textClass' => 'text-black',
+])
+
+<a href="{{ $url }}"
+    class="{{ $bgColor }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 flex items-center space-x-2">
+    @if($icon)
+        <i class="fa fa-{{ $icon }}"></i>
+    @endif
+    <span>{{ $slot }}</span>
+</a>
