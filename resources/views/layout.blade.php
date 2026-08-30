61e0654a8c6811b124b44e1e000aed75ef5fdec6
Chali Musonda <mcmusonda@gmail.com>
2026-08-30 04:54:13 +0200

    5-11 Heri Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/35c7e655a1.js" crossorigin="anonymous"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>{{ $title ?? 'Workopia | Find and List Jobs' }}</title>
 </head>
 <body class="bg-gray-100">
-    <x-header class="bg-blue-600 text-white py-4"></x-header>
+    <x-header />
+    @if(request()->is('/'))
+        <x-hero />
+    @endif
 
     <main class="container mx-auto px-4 py-8">
         {{ $slot }}
