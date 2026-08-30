3940b68954913e3ff31b616b220450207415b4ad
Chali Musonda <mcmusonda@gmail.com>
2026-08-30 04:03:55 +0200

    5-9 Mobile menu NavLink

--- resources/views/components/button-link.blade.php
+++ resources/views/components/button-link.blade.php
@@ -5,11 +5,12 @@
     'bgColor' => 'bg-yellow-500',
     'hoverClass' => 'hover:bg-yellow-600',
     'textClass' => 'text-black',
+    'block' => false,
 ])
 
 <a href="{{ $url }}"
-    class="{{ $bgColor }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 flex items-center space-x-2">
-    @if($icon)
+    class="{{ $bgColor }} {{ $hoverClass }} {{ $textClass }} px-4 py-2 rounded hover:shadow-md transition duration-300 flex items-center space-x-2 {{ $block ? 'block' : ''}}">
+    @if($icon) 
         <i class="fa fa-{{ $icon }}"></i>
     @endif
     <span>{{ $slot }}</span>
