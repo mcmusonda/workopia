04e4fffc8ad7ba3d00b9122a10526d4cb76663a8
Chali Musonda <mcmusonda@gmail.com>
2026-09-03 04:23:29 +0200

    8-9 Eloquent Relations

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -29,6 +30,12 @@ class Job extends Model
         'company_name',
         'company_description',
         'company_logo',
-        'company_website'
+        'company_website',
+        'user_id', // Add user_id to the fillable array
     ];
+
+    // Relation to the User model (assuming a job belongs to a user)
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2026_09_03_013553_add_fields_to_job_listings_table.php
+++ database/migrations/2026_09_03_013553_add_fields_to_job_listings_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         DB::table('job_listings')->truncate();
         
         Schema::table('job_listings', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+
             $table->integer('salary');
             $table->string('tags')->nullable();
             $table->enum('job_type', ['full-time', 'part-time', 'contract', 'internship'])->default('full-time');
@@ -32,6 +34,9 @@ return new class extends Migration
             $table->string('company_description')->nullable();
             $table->string('company_logo')->nullable();
             $table->string('company_website')->nullable();
+
+            // Add user foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -41,6 +46,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('job_listings', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+            
             $table->dropColumn([
                 'salary',
                 'tags',
