cfd774e3ad433825b745e69e4f085f19ac39ab93
Chali Musonda <mcmusonda@gmail.com>
2026-09-04 02:30:42 +0200

    8-3 Homepage Jobs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +10,8 @@ class HomeController extends Controller
     //
     public function index()
     {
-        return view('pages.index');
+        $jobs = Job::latest()->limit(6)->get();
+        
+        return view('pages.index')->with('jobs', $jobs);
     }
 }
