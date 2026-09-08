242f517a87450aaf572fb530ba756843ba4f066c
Chali Musonda <mcmusonda@gmail.com>
2026-09-08 16:03:44 +0200

    9-6 Register New User

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -11,5 +13,20 @@ class RegisterController extends Controller
     // @route GET /register
     public function register(): View {
         return view('auth.register');
+    }
+
+    // @desc Store user in database
+    // @route POST /register
+    public function store(Request $request): RedirectResponse {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'email', 'max:100', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed']
+        ]);
+
+        // Create User
+        $user = User::create($validatedData);
+
+        return redirect()->route('login')->with('success', 'Your registration was successful.You can now login.');
     }
 }
