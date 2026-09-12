9d5638a23aaf46163b84ebd576ee7166fac9e081
Chali Musonda <mcmusonda@gmail.com>
2026-09-12 09:45:49 +0200

    11-5 Profile Avatar Upload

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <x-alert type="success" message="{{ session('success') }}" />
+    <x-alert type="success" :message="session('success')" />
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             @forelse($jobs as $job)
