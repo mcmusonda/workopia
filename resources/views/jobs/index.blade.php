f11a81f0bbf70108a799bb1d0a40639b387308d7
Chali Musonda <mcmusonda@gmail.com>
2026-09-19 19:35:20 +0200

    14-2 Search Component & Route

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -2,6 +2,10 @@
 
     <x-alert type="success" :message="session('success')" />
 
+    <div class="bg-blue-900 h-24 px-4 mb-4 flex justify-center items-center rounded">
+        <x-search />
+    </div>
+
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
             @forelse($jobs as $job)
                 <x-job-card :job="$job" />
